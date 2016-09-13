30156cd019e13e89a7944bfeee21cfff4bbbab61
Matt <matt@profusiondev.net>
2016-09-13 10:30:30 -0600

    Fixed masonry zoom

--- js/scripts.js
+++ js/scripts.js
@@ -6,22 +6,40 @@ function filterChange(e) {
 	var queryTypes = $('input[type=checkbox]:checked').map(function() { return $(this).data("map-type"); }).get(); 
 	if(queryName != "" || queryTypes.length > 0) {
 		$('.card.filterable').hide();
-		$('.card.filterable').filter(function(){ console.log(queryTypes, $(this).data("map-type"));
+		var obj = $('.card.filterable').filter(function(){ console.log(queryTypes, $(this).data("map-type"));
 					 	   var isSameMapType = queryTypes.length == 0 || queryTypes.indexOf($(this).data("map-type")) >= 0 ;
 							return isSameMapType 
 					}).filter(function() {
 						var hasSubstringInName = queryName == "" || $(this).data("map-name").toLowerCase().indexOf(queryName) >= 0;
 						return hasSubstringInName;
-					}).show();
+					});
+		obj.show();
+		unfocusOnFirst();
+
+		var first = obj.first();
+		if(first != null){
+			console.log(first,"first");
+			focusOnFirst(first);
+		}
 	} else {
+		unfocusOnFirst();
 		$('.card.filterable').show();
 	}
 	refreshMasonry();
 }
 
+function unfocusOnFirst(e){
+	$(".m12 .card.filterable").parent().removeClass("m12").addClass("m6");
+}
+
+function focusOnFirst(e){
+	console.log()
+	e.parent().removeClass("m6").addClass("m12");
+}
+
 function refreshMasonry() {
 	$('#parentMason').masonry({
-	  itemSelector: '.col'
+	  itemSelector: '.col',percentPosition: true, columnWidth: '.m6'
 	});
 }
 
